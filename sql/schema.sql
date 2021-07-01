1972e21f743e35dd34ec7d3440907034b1490b62
David Vasek <david.vasek51@gmail.com>
2021-07-01 15:29:08 -0500

    changed seed and schema file

--- sql/schema.sql
+++ sql/schema.sql
@@ -1,14 +1,16 @@
-CREATE TABLE toDoList (
-    id serial PRIMARY KEY,
-    task text NOT NULL,
-    user_id integer REFERENCES users (id)
-    task_complete NOT NULL DEFAULT FALSE
-    
-);
-
 CREATE TABLE users (
     id serial PRIMARY KEY,
     name varchar(100),
     email varchar(200),
     password varchar(2000)
 );
+
+CREATE TABLE toDoList (
+    id serial PRIMARY KEY,
+    task varchar(2000),
+    user_tasks integer REFERENCES users (id),
+    status boolean NOT NULL 
+    
+);
+
+
