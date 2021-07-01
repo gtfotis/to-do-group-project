b7f3077233af5952c0d23ec5d57d12e59892b45a
otis wilcox <otiswilcox@gmail.com>
2021-07-01 16:30:13 -0400

    added log in function, working on is_logged_in functions

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const hostname = '127.0.0.1';
 const port = 3000;
 const express = require('express');
 const app = express();
+const session = require('express-session')
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static('public'));
@@ -11,14 +12,21 @@ const es6renderer = require('express-es6-template-engine');
 app.engine('html', es6renderer);
 app.set('views', './views');
 app.set('view engine', 'html');
+app.use(
+    session({
+        secret: 'sean rools',
+        resave: false,
+        saveUninitialized: false,
+        is_logged_in: false,
+    })
+);
 const server = http.createServer(app);
 server.listen(port, hostname, () => {
-    console.log()
+    console.log(`The server is running at http://${hostname}:${port}.`);
 });
 
 const rootController = require('./routes/index');
-const toDoController = require('./routes/to-do');
-const registerController = require('./routes/register');
+// const toDoController = require('./routes/to-do');
+
 app.use('/', rootController);
-app.use('/to-do', toDoController);
-app.use('/register', registerController);
+// app.use('/to-do', toDoController);
