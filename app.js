f797b4f7b024efd9134bef070826eee7b1db6f73
otis wilcox <otiswilcox@gmail.com>
2021-07-01 14:54:16 -0400

    added controllers for the to-do page and registration page (root is our log-in page)

--- app.js
+++ app.js
@@ -4,11 +4,6 @@ const hostname = '127.0.0.1';
 const port = 3000;
 const express = require('express');
 const app = express();
-const helmet = require('helmet');
-app.use(helmet());
-const morgan = require('morgan');
-const logger = morgan('tiny');
-app.use(logger);
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static('public'));
@@ -20,5 +15,10 @@ const server = http.createServer(app);
 server.listen(port, hostname, () => {
     console.log()
 });
+
 const rootController = require('./routes/index');
+const toDoController = require('./routes/to-do');
+const registerController = require('./routes/register');
 app.use('/', rootController);
+app.use('/to-do', toDoController);
+app.use('/register', registerController);
